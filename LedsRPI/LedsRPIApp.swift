a6bccb7a823c90c580c82af534f405c0883c03cb
ondrejkondek <ondrej.kondek@gmail.com>
2023-09-16 14:03:01 +0200

    Initial Commit

---
+++ LedsRPI/LedsRPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedsRPIApp.swift
+//  LedsRPI
+//
+//  Created by Ondrej Kondek on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LedsRPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
